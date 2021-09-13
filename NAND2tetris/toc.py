7ec2c52a7f4ef62f6a0d02b87c0588889051bd17
muugumuugu <mogimogi120501@gmail.com>
2021-09-13 21:47:59 +0530

    README

--- NAND2tetris/toc.py
+++ NAND2tetris/toc.py
@@ -7,6 +7,6 @@ printt=['TOC\n', '---\n' ,'|Lec ID| Lec Name|\n' ,'| ---| --- |\n']
 for line in lines:
 	title=re.sub(r'## UNIT \d+\.\d+','',line.strip())
 	idd=title=re.sub(r'(## UNIT \d+\.\d+).*$',r'\1',line.strip())
-	toprint='|' +  idd + '|' + '['+ title  +']'+ '(notes.md#' + line.strip().replace(' ','-')  +')'+ '|\n'
+	toprint='|' +  idd + '|' + '['+ title  +']'+ '(notes.md#' + line.strip().replace(' ','-').replace('##','')  +')'+ '|\n'
 	printt.append(toprint);
 fh.writelines(printt)
